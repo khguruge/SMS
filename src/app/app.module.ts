34ca22902bc7b41667fd19ccf31f83184838c9b0
khguruge <gkasunharshana@gmail.com>
2020-07-12 21:15:12 +0530

    test

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,16 +5,27 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ElishCustomMaterialModule } from './shared/custom.material'
+import { MatSliderModule } from '@angular/material/slider';
+import { LoginComponent } from './shared/login/login.component';
+import { SignupComponent } from './shared/signup/signup.component';
+import { HeaderComponent } from './shared/header.component';
+import { TestComponent } from './test/test.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    SignupComponent,
+    HeaderComponent,
+    TestComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    ElishCustomMaterialModule
+    ElishCustomMaterialModule,
+    MatSliderModule
   ],
   providers: [],
   bootstrap: [AppComponent]
